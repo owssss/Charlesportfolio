5b512a46a1419fcb6971ed71b184a51c70998a9e
Charles <132845311+owssss@users.noreply.github.com>
2023-05-26 23:04:40 +0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -96,16 +96,5 @@
             $('#more-projects').fadeIn(300);
         });
     });
-    
-    //slick carousel
-    $(document).ready(function() {
-    $('.project-carousel').slick({
-        dots: true,
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1
-    });
-});
-
 
 })(jQuery);
